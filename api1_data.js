37ebff498ef63298abf4ca2f1834cc0e71d66ecc
Chinteshdabhade27 <chinteshdabhade@27gmail.com>
2024-07-26 18:21:01 +0530

    my commit

---
+++ api1_data.js
@@ -0,0 +1,13 @@
+const data=[
+    {name:'akash',age:22,sallary:2000,city:'amravati'},
+    {name:'chintesh',age:23,sallary:2000,city:'amravati'},
+    {name:'rohit',age:22,sallary:2000,city:'amravati'},
+    {name:'raghav',age:25,sallary:2000,city:'amravati'},
+    {name:'abhinav',age:24,sallary:2000,city:'amravati'},
+    {name:'aditya',age:22,sallary:2000,city:'amravati'},
+    {name:'rupali',age:32,sallary:2000,city:'amravati'},
+    {name:'radhika',age:22,sallary:2000,city:'amravati'},
+    {name:'priti',age:42,sallary:2000,city:'amravati'},
+    {name:'ram',age:22,sallary:2000,city:'amravati'},
+];
+module.exports=data;
